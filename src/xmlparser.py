a78c895f2bc34738248a7543b4fa71a2ed5851ea
odeveci <odeveci@sutdent.tgm.ac.at>
2017-12-20 19:53:35 +0100

    finished simple version of xml parser

--- src/xmlparser.py
+++ src/xmlparser.py
@@ -1,5 +1,5 @@
 from xml.etree import ElementTree as et
-import sys
+
 
 class XMLParser():
 
@@ -9,7 +9,29 @@ class XMLParser():
 
     def parse(self):
         doc = et.parse(self.filename)
-        print(et.tostring(doc.getroot()).decode('utf-8'))
+        #print(et.tostring(doc.getroot()).decode('utf-8'))
+        print("Weclome to the bookstore!")
+        count=0
+        #Alle <book> ... </book> Elemente, also jedes Buch-Element, werden durchiteriert
+        for element in doc.findall("book"):
+            #Alle Strings in Kindeselementen von <book> werden herausgehoben mit find().text  , mit strip() werden Whitespaces am Anfang und Ende des Strings entfernt.
+            author = element.find("author").text.strip()
+            title = element.find("title").text.strip()
+            year = element.find("year").text.strip()
+            price = element.find("price").text.strip()
+            #Attribut in Kind-Element <title> wird geholt und zwar 'lang'
+            language = element.find("title").attrib.get('lang')
+            #find() für Attribut <book> nicht nötig, da dies schon in element enthalten ist
+            category = element.attrib.get('category')
+            #Gespeicherte Elemente werden formatiert ausgegeben
+            print("Book %d \n Title: %s \n Author: %s \n Year: %s \n Price: %s \n Language: %s \n Category: %s \n " %
+            (count,title,author,year,price,language,category))
+            count+=1
+
+
+
+
+
 
 
 
